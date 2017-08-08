f279d9d020296623b5e14025180f3476de9a82bf
Lee <leechon@amazon.com>
2017-08-08 17:42:22 +0900

    ...

--- src/main.php
+++ src/main.php
@@ -1,21 +1,38 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Welcome to the AWS PoC for octank</title>
-    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
-  </head>
+<html lang="en">
 
-  <body>
-    <div id="wrapper">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Welcome to OCTANK</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <style>body {margin-top: 40px; background-color: #777;}</style>
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
 
-      <?php include('menu.php'); ?>
+    <body>
+        <div class="container">
+            <div class="hero-unit" >
+                <h1>OCTANK WEB PoC</h1>
+                <h2>Welcome to OCTANK Web Service</h2>
+                <p>This PHP application is now running on a container in Amazon ECS.</p>
+                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <?php
+                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                        echo fread($myfile,filesize("/var/www/my-vol/date"));
+                        fclose($myfile);
+                ?>
+                <h2><?php include('get-cpu-load.php'); ?></h2>
+                <p> CPU LOAD </P>
+                <P> RDS </P>
 
-      <?php include("get-index-meta-data.php"); ?>
+            </div>
+        </div>
 
-      <hr />
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
 
-      <?php include('get-cpu-load.php'); ?>
-
-    </div>
-  </body>
 </html>
