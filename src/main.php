496d719d0b85d13f144cf52c612a5937ef12177f
Lee <leechon@amazon.com>
2017-08-09 13:43:00 +0900

    ..8..

--- src/main.php
+++ src/main.php
@@ -27,6 +27,7 @@
                 ?>
                 <h2><?php include('get-cpu-load.php'); ?></h2>
                 <br />
+                <p><h2><a href="octank-photo.php" style="text-decoration:none">OCTANK Logo</a></h2></P>
                 <p><h2><a href="put-cpu-load.php" style="text-decoration:none">CPU LOAD >></a></h2></P>
                 <P><h2><a href="rds-read-data.php" style="text-decoration:none">RDS >></a></h2></P>
             </div>
