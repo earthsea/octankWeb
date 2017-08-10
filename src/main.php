8d58bdb2aa93c3b514445f92bd09bda37f720d0b
Lee <leechon@amazon.com>
2017-08-10 16:02:31 +0900

    ..12..

--- src/main.php
+++ src/main.php
@@ -8,7 +8,7 @@
         <title>Welcome to OCTANK</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #777;}</style>
+        <style>body {margin-top: 40px; background-color: #700;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
